fc4a3fe302c4c068a73697adf414a8a7f1f914ea
Scott Dixon <scott.dixon@shopify.com>
2022-02-14 11:09:50 +1000

    Wrap product page in layout

--- src/pages/products/[handle].server.jsx
+++ src/pages/products/[handle].server.jsx
@@ -1,4 +1,11 @@
+import Layout from '../../components/Layout.server';
+
 export default function Product({params}) {
   const {handle} = params;
-  return <p>Product page for: {handle}</p>;
+
+  return (
+    <Layout>
+      <p>Product page for: {handle}</p>
+    </Layout>
+  );
 }
