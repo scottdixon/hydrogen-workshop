0272b269fef4e48ebfc3d55cda8eed4ad6a8648e
Scott Dixon <scott.dixon@shopify.com>
2022-02-14 12:51:30 +1000

    Prouct page price, image and add to cart button

--- src/components/ProductPage.client.jsx
+++ src/components/ProductPage.client.jsx
@@ -1,11 +1,31 @@
-import {Product, flattenConnection} from '@shopify/hydrogen/client';
+import {Product, flattenConnection, useProduct} from '@shopify/hydrogen/client';
+import {BUTTON_PRIMARY_CLASSES} from './Button.client';
 export default function ProductPage({product}) {
   const initialVariant = flattenConnection(product.variants)[0];
 
   return (
     <Product product={product} initialVariantId={initialVariant.id}>
+      <Product.SelectedVariant.Image />
       <Product.Title />
       <Product.Description />
+      <Product.SelectedVariant.Price />
+      <AddToCartMarkup />
     </Product>
+  );
+}
+
+function AddToCartMarkup() {
+  const {selectedVariant} = useProduct();
+  const isOutOfStock = !selectedVariant.availableForSale;
+
+  return (
+    <div className="space-y-2 mb-8">
+      <Product.SelectedVariant.AddToCartButton
+        className={BUTTON_PRIMARY_CLASSES}
+        disabled={isOutOfStock}
+      >
+        {isOutOfStock ? 'Out of stock' : 'Add to bag'}
+      </Product.SelectedVariant.AddToCartButton>
+    </div>
   );
 }
