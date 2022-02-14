4e1f2429d44678e4c195b18e85b6385820535969
Scott Dixon <scott.dixon@shopify.com>
2022-02-14 11:17:37 +1000

    Product page initial GraphQL query

--- src/pages/products/[handle].server.jsx
+++ src/pages/products/[handle].server.jsx
@@ -1,7 +1,21 @@
 import Layout from '../../components/Layout.server';
-
+import gql from 'graphql-tag';
+import {useShopQuery} from '@shopify/hydrogen';
 export default function Product({params}) {
   const {handle} = params;
+
+  const {data} = useShopQuery({
+    query: gql`
+      query ProductPage {
+        product(handle: "snowboard") {
+          id
+          title
+        }
+      }
+    `,
+  });
+
+  console.log(data);
 
   return (
     <Layout>
